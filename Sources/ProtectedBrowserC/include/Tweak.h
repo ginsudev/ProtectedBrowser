b7731e79b029a5bbcbf00ad8693637352b44c4fe
Ginsu <noahlittle199@gmail.com>
2022-08-20 15:07:08 +1000

    Patch

--- Sources/ProtectedBrowserC/include/Tweak.h
+++ Sources/ProtectedBrowserC/include/Tweak.h
@@ -1,14 +1,5 @@
 #import <UIKit/UIKit.h>
 #import <WebKit/WebKit.h>
-#import <SafariServices/SafariServices.h>
-
-@interface WKWebView (Private)
-@property (nonatomic,readonly) NSURL * _mainFrameURL;
-@end
-
-@interface UIView (Private)
-- (UIViewController *)_viewControllerForAncestor;
-@end
 
 @interface WKUserContentController (Private)
 - (void)removeAllContentRuleLists;
